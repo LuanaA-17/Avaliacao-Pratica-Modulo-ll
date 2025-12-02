089015a6c51bb63911eca2cb47b93a96ac1fb957
LuanaA-17 <luanaalencarcarvalhoalves@gmail.com>
2025-12-02 16:33:25 -0300

    Add files via upload

---
+++ Avaliacao-Pratica-Modulo-ll-main/app_flask.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+## Continue o código aqui.
+
+@app.route("/soma")
+def soma():
+    v1 =int(request.args.get("valor1"))
+    v2 =int(request.args.get("valor2"))
+    return f'{v1} + {v2} = {v1 + v2}'
+
+@app.route("/subtrair")
+def subtrair():
+    v1 =int(request.args.get("valor1"))
+    v2 =int(request.args.get("valor2"))
+    return f'{v1} - {v2} = {v1 - v2}'
+
+@app.route("/multiplicar")
+def multiplicar():
+    v1 =int(request.args.get("valor1"))
+    v2 =int(request.args.get("valor2"))
+    return f'{v1} * {v2} = {v1 * v2}'
+
+@app.route("/dividir")
+def dividir():
+    v1 =int(request.args.get("valor1"))
+    v2 =int(request.args.get("valor2"))
+    return f'{v1} / {v2} = {v1 / v2}'
+
+if __name__ == "__main__":
+    app.run(debug=True)
